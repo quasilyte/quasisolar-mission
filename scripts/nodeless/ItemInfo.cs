f8f87d2167d311e6cad1de6be534f2c0375c0c26
Iskander Sharipov <i.sharipov@corp.vk.com>
2021-05-29 23:59:32 +0300

    fix nullptr in quick battle when needle gun is used

--- scripts/nodeless/ItemInfo.cs
+++ scripts/nodeless/ItemInfo.cs
@@ -95,7 +95,7 @@ public class ItemInfo {
 
     private static int ItemPrice(IItem item, bool selling) {
         if (item is WeaponDesign weapon) {
-            if (!selling && weapon == NeedleGunWeapon.Design && RpgGameState.instance.technologiesResearched.Contains("Gauss Production")) {
+            if (RpgGameState.instance != null && !selling && weapon == NeedleGunWeapon.Design && RpgGameState.instance.technologiesResearched.Contains("Gauss Production")) {
                 return (int)(weapon.sellingPrice * 0.8);
             }
             return weapon.sellingPrice;
