8cee5dee170cf818681c9702dee402217f176653
Iskander Sharipov <i.sharipov@corp.vk.com>
2021-09-07 21:31:29 +0300

    improve energy converter artifact

--- scripts/nodeless/artifacts/EnergyConverterArtifact.cs
+++ scripts/nodeless/artifacts/EnergyConverterArtifact.cs
@@ -4,13 +4,13 @@ public class EnergyConverterArtifact : IArtifact {
     public static ArtifactDesign Design = new ArtifactDesign{
         name = "Energy Converter",
         description = "Redirects otherwise wasted produced energy into the backup batteries",
-        effect = "+1 backup energy/sec when primary energy is full",
+        effect = "+2 backup energy/sec when primary energy is full",
         sellingPrice = 2500,
     };
 
     public void Apply(VesselState state, float delta) {
         if (state.energy >= state.stats.maxEnergy) {
-            state.backupEnergy += delta;
+            state.backupEnergy += delta * 2;
             if (state.backupEnergy >= state.stats.maxBackupEnergy) {
                 state.backupEnergy = state.stats.maxBackupEnergy;
             }
