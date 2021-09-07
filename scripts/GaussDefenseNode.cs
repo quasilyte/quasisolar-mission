bffb3530cef783dc22c20380d1bdaa31b7de72d9
Iskander Sharipov <i.sharipov@corp.vk.com>
2021-09-07 21:26:18 +0300

    fix gauss turret bug

--- scripts/GaussDefenseNode.cs
+++ scripts/GaussDefenseNode.cs
@@ -4,22 +4,27 @@ using System;
 public class GaussDefenseNode : Node2D {
     private Pilot _target;
 
-    private int _alliance;
+    private Pilot _owner;
     private int _burst = 3;
     private float _burstCooldown = 0;
 
     private static PackedScene _scene = null;
-    public static GaussDefenseNode New(int alliance, Pilot target) {
+    public static GaussDefenseNode New(Pilot owner, Pilot target) {
         if (_scene == null) {
             _scene = GD.Load<PackedScene>("res://scenes/GaussDefenseNode.tscn");
         }
         var o = (GaussDefenseNode)_scene.Instance();
-        o._alliance = alliance;
+        o._owner = owner;
         o._target = target;
         return o;
     }
 
     public override void _Process(float delta) {
+        if (!IsInstanceValid(_target.Vessel)) {
+            QueueFree();
+            return;
+        }
+
         _burstCooldown -= delta;
         if (_burstCooldown < 0) {
             _burstCooldown = 0;
@@ -29,8 +34,7 @@ public class GaussDefenseNode : Node2D {
             _burst--;
             _burstCooldown += 0.5f;
 
-            var dummyPilot = new Pilot{alliance = _alliance};
-            var projectile = Projectile.New(NeedleGunWeapon.TurretDesign, dummyPilot);
+            var projectile = Projectile.New(NeedleGunWeapon.TurretDesign, _owner);
             var predictedPos = QMath.CalculateSnipeShot(NeedleGunWeapon.TurretDesign, Position, _target.Vessel.Position, _target.Vessel.State.velocity);
             var cursor = QMath.RandomizedLocation(predictedPos, 14);
             projectile.Position = Position + new Vector2(0, QRandom.FloatRange(-15, 15));
