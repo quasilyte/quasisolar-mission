1b9d70d1daa814d6dc316847221e24033be38de0
Iskander Sharipov <i.sharipov@corp.vk.com>
2021-09-07 21:18:16 +0300

    utils: add FactionColor function

--- scripts/nodeless/Utils.cs
+++ scripts/nodeless/Utils.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using System.Collections.Generic;
+using Godot;
 
 public static class Utils {
     private static Dictionary<string, int> _decimalToRoman = new Dictionary<string, int>{
@@ -33,7 +34,12 @@ public static class Utils {
         var paragraphs = new List<string>();
         string p = "";
 
-        foreach (var l in text.Split('\n')) {
+        var lines = text.Split('\n');
+        if (lines.Length == 1) {
+            return lines[0];
+        }
+
+        foreach (var l in lines) {
             var s = l.Trim();
             if (string.IsNullOrEmpty(s)) {
                 if (!string.IsNullOrEmpty(p)) {
@@ -52,5 +58,33 @@ public static class Utils {
 
         var resultString = string.Join("\n\n", paragraphs);
         return resultString.Replace("\n ", "\n");;
+    }
+
+    public static Color FactionColor(Faction faction) {
+        if (faction == Faction.Earthling) {
+            return Color.Color8(0x82, 0xd5, 0xd5);
+        }
+        if (faction == Faction.Wertu) {
+            return Color.Color8(0x16, 0x4b, 0xb6);
+        }
+        if (faction == Faction.Krigia) {
+            return Color.Color8(0xca, 0x30, 0x4d);
+        }
+        if (faction == Faction.Zyth) {
+            return Color.Color8(0x15, 0xad, 0x27);
+        }
+        if (faction == Faction.Phaa) {
+            return Color.Color8(0x88, 0xc1, 0x19);
+        }
+        if (faction == Faction.Draklid) {
+            return Color.Color8(0x89, 0x1d, 0xcd);
+        }
+        if (faction == Faction.Vespion) {
+            return Color.Color8(0x83, 0x9c, 0xd5);
+        }
+        if (faction == Faction.Rarilou) {
+            return Color.Color8(0xdf, 0xa6, 0x24);
+        }
+        throw new System.Exception("unexpected faction: " + faction.ToString());
     }
 }
