e7f60d63569d96f91b1856887ad8157b0aec358a
Iskander Sharipov <i.sharipov@corp.vk.com>
2021-09-25 01:15:18 +0300

    make save/load work on Android too

--- scripts/state/GameStateSerializer.cs
+++ scripts/state/GameStateSerializer.cs
@@ -6,11 +6,12 @@ using Newtonsoft.Json;
 
 public static class GameStateSerializer {
     public static void Encode(RpgGameState gameState) {
-        GD.Print("encoding to " + System.Environment.CurrentDirectory + "/gamedata.json");
+        var path = ProjectSettings.GlobalizePath("user://gamedata.json");
+        GD.Print("encoding to " + path);
 
         string json = JsonConvert.SerializeObject(gameState, Formatting.Indented);
 
-        using (FileStream fs = new FileStream("gamedata.json", FileMode.Create)) {
+        using (FileStream fs = new FileStream(path, FileMode.Create)) {
             var utf8 = new UTF8Encoding();
             var data = utf8.GetBytes(json);
             fs.Write(data, 0, data.Length);
@@ -18,9 +19,10 @@ public static class GameStateSerializer {
     }
 
     public static RpgGameState Decode() {
-        GD.Print("decoding from " + System.Environment.CurrentDirectory + "/gamedata.json");
+        var path = ProjectSettings.GlobalizePath("user://gamedata.json");
+        GD.Print("decoding from " + path);
 
-        using (StreamReader r = new StreamReader("gamedata.json")) {
+        using (StreamReader r = new StreamReader(path)) {
             string json = r.ReadToEnd();
             var gameState = JsonConvert.DeserializeObject<RpgGameState>(json);
             return gameState;
